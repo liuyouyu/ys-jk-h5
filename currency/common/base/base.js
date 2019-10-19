876dd32cf94e8b65fadb954c8c37da36eafc2607
malinxiaM <770879122@qq.com>
2019-10-19 20:13:40 +0800

    更改appCode,serviceCode-马林霞-2019年10月19日20:13:32

--- currency/common/base/base.js
+++ currency/common/base/base.js
@@ -31,9 +31,9 @@ mcMethod.url = {
 
 mcMethod.info = {
   companyId: mcMethod.getRequestValue('companyId') || 'root',
-  appCode: mcMethod.getRequestValue('appCode') || 'emp',
+  appCode: mcMethod.getRequestValue('appCode') || 'VLOG_YUNSHI',
   userId: mcMethod.getRequestValue('userId') || '5d8dc8ad05a9b43d88fb3b7f',
-  serviceCode:mcMethod.getRequestValue('serviceCode') || 'cloudemp',
+  serviceCode:mcMethod.getRequestValue('serviceCode') || 'YUNSHI_XSGL',
   versionId: mcMethod.getRequestValue('versionId')  || '1',
   activityId: mcMethod.getRequestValue('activityId') || '',
   activityTemplateId: mcMethod.getRequestValue('activityTemplateId') || ''
