de4470ee2e2f5ee29e74d96dbc7a8d00c79eac0d
malinxia <malinxia@cdvcloud.com>
2019-10-31 21:39:39 +0800

    更改域名配置-马林霞-2019年10月31日21:39:31

--- currency/config/config.js
+++ currency/config/config.js
@@ -1,5 +1,5 @@
 var CONFIG = {
-  cloudUrl: 'http://test-empapi.yunshicloud.com/',
+  cloudUrl: 'https://test-empapi.yunshicloud.com/',
   //短信验证服务
   showCatalogue: true,  //是否显示推荐内容 (没有推荐内容数据时，true也不会显示)
   //h5分享---文稿访问域名
