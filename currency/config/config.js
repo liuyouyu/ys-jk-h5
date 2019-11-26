a2d29feac4df81cc659ec0535b8bef6299016612
malinxia <770879122@qq.com>
2019-11-26 16:46:18 +0800

    隐藏推荐内容-马林霞

--- currency/config/config.js
+++ currency/config/config.js
@@ -1,7 +1,7 @@
 var CONFIG = {
   cloudUrl: 'https://test-empapi.yunshicloud.com/',
   //短信验证服务
-  showCatalogue: true,  //是否显示推荐内容 (没有推荐内容数据时，true也不会显示)
+  showCatalogue: false,  //是否显示推荐内容 (没有推荐内容数据时，true也不会显示)
   //h5分享---文稿访问域名
   apiHost: 'https://test-cdapi.yunshicloud.com/',
 }
